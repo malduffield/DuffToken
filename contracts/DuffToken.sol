b6b3566bde8c80ab6d3c654f67528f1dcff874e4
malduffield <33203640+malduffield@users.noreply.github.com>
2021-04-05 23:40:31 +0100

    2. Transfer Tokens

--- contracts/DuffToken.sol
+++ contracts/DuffToken.sol
@@ -6,11 +6,37 @@ contract DuffToken {
   string public standard = "Duff Token v1.0";
   uint256 public totalSupply;
 
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+    );
+
   mapping(address => uint256) public balanceOf;
 
   constructor (uint256 _initialSupply) public {
     // allocate initial supply
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
+  }
+
+  // transfer
+  // exception
+  // return a boolean
+  // transfer event
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    // exception if account doesnt have enough
+    // require ketyword saying if this is true carry on else fail.
+    require(balanceOf[msg.sender] >= _value);
+
+    // transfer the balance from msg.sender to address passed in
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    // transfer event
+    emit Transfer(msg.sender, _to, _value);
+
+    // return a boolean
+    return true;
   }
 }
